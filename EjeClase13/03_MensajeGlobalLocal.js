ff2525ed535eda0c4dfe8759ecadf13c9f7b6e4a
LuciaGuerraDiaz <gd.lucia@gmail.com>
2026-01-02 19:52:06 -0300

    Agregue el saludo local al código

--- EjeClase13/03_MensajeGlobalLocal.js
+++ EjeClase13/03_MensajeGlobalLocal.js
@@ -3,7 +3,22 @@
 //Scope Global, se declara fuera de cualquier funcion y existe en todo el codigo.
 let saludoGlobal = 'Hola desde el Scope global';
 function mostrarGlobal () {
+    let saludoLocal = "Soy el local"
+    console.log(saludoLocal);
+};
+mostrarGlobal(); //Esto muestra la opción del saludo local invocando la funcion
+
+//Segunda forma de ver el saludo local con return
+
+function mostrarGlobal2 () {
+
+    let local = "Soy el saludo local usando return";
+    return local
 }
+
+let mensajelocal2 = mostrarGlobal2 ();
+console.log(mensajelocal2);
+
 // Imprime: Soy una variable global
-console.log(saludoGlobal); 
-// Ejemplo de scope de bloque
+console.log(saludoGlobal);
+
